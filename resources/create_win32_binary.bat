6bf0851a500b64cd85a5059d34f03dc2331c0ad2
stefanhen83 <stefanhen83@52bf4e17-6a1c-0410-83a1-9b5866916151>
2011-06-14 20:13:25 +0000

    add some resources

--- resources/create_win32_binary.bat
+++ resources/create_win32_binary.bat
@@ -1,7 +1,7 @@
 @echo off
 IF "%D2TM_HOME%"=="" goto D2TM_HOME_NOT_SET
 cd %D2TM_HOME%
-cls 
+cls
 echo.
 echo Prepare binary package (./bin) of D2TM. 
 echo.
@@ -28,15 +28,20 @@ copy resources\campaign\maps\*.* bin\campaign\maps
 copy resources\campaign\maps\seed\*.* bin\campaign\maps\seed
 copy resources\campaign\briefings\*.* bin\campaign\briefings
 copy resources\campaign\info.txt bin\campaign\info.txt
+
 echo 5. Creating new data directory
 mkdir bin\data
-mkdir bin\data\scenes
-echo 6. Copying data files
-copy resources\data\*.dat bin\data
-copy resources\data\*.fon bin\data
+xcopy /I /D /E "resources/data" "../bin/data"
+
+echo 6. Copying to data
+copy resources\data\*.fon bin\data\font
+copy resources\data\*.ttf bin\data\font
+
 copy resources\data\*.ttf bin\data
+
 echo 7. Copying scenes
 copy resources\data\scenes\*.* bin\data\scenes
+
 echo 8. Create save game directories
 mkdir bin\save
 mkdir bin\save\atreides
@@ -47,9 +52,9 @@ echo 9. Create skirmish directory
 mkdir bin\skirmish
 copy resources\skirmish\*.ini bin\skirmish
 echo 10. Copying dll files
-copy resources\dll\mingw32\*.* bin
-echo 11. Copy executable from %1
-copy Default\*.exe bin
+copy resources\dll\*.* bin
+echo 11. Copy executable from
+copy Release\*.exe bin
 echo 12. Copy game rules file (game.ini)
 copy resources\game.ini bin
 echo 13. Copy txt files
@@ -67,6 +72,7 @@ echo Finished with errors.
 goto END
 
 :FINISHED_SUCCESS
+cd bin
 echo Done.
 
 :END
