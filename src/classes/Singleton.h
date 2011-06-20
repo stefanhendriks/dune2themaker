cebf45796dc6be9d1adf7cbb448ce2a537f44c16
peter.gaal.sk@gmail.com <peter.gaal.sk@gmail.com@52bf4e17-6a1c-0410-83a1-9b5866916151>
2011-06-20 21:54:25 +0000

    repaired template syntax to be compatible with mingw compiler

--- src/classes/Singleton.h
+++ src/classes/Singleton.h
@@ -6,17 +6,19 @@ template<class C>
 class Singleton {
 	public:
 		static C* Create() {
+			// should be zero here, if we call second time, we will have a memory leak
 			assert(!Singleton<C>::m_Instance);
 			Singleton<C>::m_Instance = ::new C;
 			return Singleton<C>::m_Instance;
 		}
 		static void Destroy() {
+			// shouldn't be zero here, otherwise access violation
 			assert(Singleton<C>::m_Instance);
 			delete Singleton<C>::m_Instance;
 			m_Instance = 0;
 		}
 		static C* GetInstance() {
-			// shouldn't be zero here
+			// shouldn't be zero here, otherwise possible access violation
 			assert(Singleton<C>::m_Instance);
 			return m_Instance;
 		}
@@ -24,6 +26,6 @@ class Singleton {
 		static C* m_Instance;
 };
 
-#define DefSingleton(C) C* Singleton<C>::m_Instance = 0;
+#define DefSingleton(C) template <> C* Singleton<C>::m_Instance = 0;
 
 #endif // _SINGLETON_H_
