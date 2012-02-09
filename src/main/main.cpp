0492688f05b4bc95eee12f366882c851561dacc8
stefanhen83@gmail.com <stefanhen83@gmail.com@52bf4e17-6a1c-0410-83a1-9b5866916151>
2012-02-09 13:22:06 +0000

    Add gamefactory, and use it in main

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,7 @@
-#include "game.h"
+#include "gamefactory.h"
 
 int main(int argc, char **argv) {
-	Game game;
+	GameFactory gameFactory;
+	Game game = gameFactory.create();
 	return game.execute();
 }
