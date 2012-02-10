1ff6116c703142d4c6a63a408f5c15db49c8cd32
stefanhen83@gmail.com <stefanhen83@gmail.com@52bf4e17-6a1c-0410-83a1-9b5866916151>
2012-02-10 21:28:36 +0000

    Draw surfaces with colorkey (transparant)

--- src/main/surfacedrawer.cpp
+++ src/main/surfacedrawer.cpp
@@ -13,6 +13,13 @@ void SurfaceDrawer::draw(SDL_Surface * from, SDL_Surface * dest, int x, int y) {
 	SDL_BlitSurface(from, NULL, dest, &rect);
 }
 
+void SurfaceDrawer::drawTransparant(SDL_Surface * from, SDL_Surface * dest, int x, int y) {
+ 	Uint32 colorkey = SDL_MapRGB(from->format, 255, 0, 255);
+    SDL_SetColorKey(from, SDL_SRCCOLORKEY, colorkey);
+    draw(from, dest, x, y);
+    SDL_SetColorKey(from, 0, 0); 
+}
+
 void SurfaceDrawer::clearToColor(SDL_Surface * target, Uint32 color) {
 	if (target == NULL) return;
 	SDL_FillRect (target, NULL, color); 	
