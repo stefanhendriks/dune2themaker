aee3e0d4c0b2102914580971bce14788452eec47
stefanhen83@gmail.com <stefanhen83@gmail.com@52bf4e17-6a1c-0410-83a1-9b5866916151>
2012-02-14 12:11:01 +0000

    Let map camera take the map it views into account, respecting the boundaries.

--- src/main/cpp/mapcamera.cpp
+++ src/main/cpp/mapcamera.cpp
@@ -1,19 +1,37 @@
 #include "mapcamera.h"
 
-MapCamera::MapCamera() {
-	x = y = 0;
-}
-
-MapCamera::MapCamera(int x, int y) {
-	this->x = x;
-	this->y = y;
+MapCamera::MapCamera(Map * map, int viewportWidth, int viewportHeight) {
+	this->x = 0;
+	this->y = 0;
+	this->viewportWidth = viewportWidth;
+	this->viewportHeight = viewportHeight;
+	this->map = map;
 }
 
 MapCamera::~MapCamera() {
-	
+	map = NULL;
 }
 
 void MapCamera::moveTo(int x, int y) {
 	this->x = x;
 	this->y = y;
+	handleMapBoundaries();
+}
+
+void MapCamera::handleMapBoundaries() {
+	if (map == NULL)	return;
+
+	int outerX = x + viewportWidth;
+	int outerY = y + viewportHeight;
+
+	if (outerX > map->getWidth()) {
+		x = map->getWidth() - viewportWidth;
+	}
+
+	if (outerY > map->getHeight()) {
+		y = map->getHeight() - viewportHeight;
+	}
+
+	if (x < 0) x = 0;
+	if (y < 0) y = 0;
 }
