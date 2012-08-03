f7ef5f43a6cbd5bdc7f1503190e530ab7f4cab44
ehalls@gmail.com <ehalls@gmail.com@52bf4e17-6a1c-0410-83a1-9b5866916151>
2012-08-03 01:35:39 +0000

    Converting from Allegro to SDL for porting to BlackBerry PlayBook

---
+++ src/main/cGame.h
@@ -0,0 +1,54 @@
+/*
+
+ Dune II - The Maker
+
+ Author : Stefan Hendriks
+ Contact: stefanhen83@gmail.com
+ Website: http://dune2themaker.fundynamic.com
+
+ 2001 - 2011 (c) code by Stefan Hendriks
+
+ */
+#ifndef GAME_H_H
+#define GAME_H_H
+
+#include "gameobjects/ScreenResolution.h"
+
+#include "include/GameState.h"
+
+#include "states/State.h"
+
+class cGame {
+
+	public:
+
+		cGame();
+		~cGame();
+
+		std::string game_filename;
+
+		bool windowed;
+		char version[15];
+		int revision;
+
+		// init functions
+		bool setupGame();
+		void init();
+
+		// main run function
+		void run();
+
+		void shutdown();
+
+		State * getState() { return state; }
+
+	private:
+
+		// variables
+		GameState gameStateEnum;
+		State * state;
+		ScreenResolution * screenResolution;
+
+};
+
+#endif
